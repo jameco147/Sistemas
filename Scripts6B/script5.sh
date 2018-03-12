e492b374aff34c8f6e10348349db2860e2a2f3d7
Jameco147 <jmerinocoll@gmail.com>
2018-03-12 23:16:23 +0100

    Scripts

--- Scripts6B/script5.sh
+++ Scripts6B/script5.sh
@@ -1,17 +1,23 @@
 contador=0
 contador2=0
-suma=0
+procesos=0
+procesos2=0
+v1=`cat listado.txt | wc -l`
 
-for i in $(cat listado.txt | awk '{print $2}'); do
-    if [ $i = Windows ];then
+
+for i in `seq 1 $v1`; do
+    v2=`cat listado.txt | head -$i | tail -1 | awk '{print $2}'`
+
+    if [ $v2 = Windows ];then
 	contador=$((contador + 1))
+	procesos=$((procesos + `cat listado.txt | head -$i | tail -1 | awk '{print $3}'`))
+    
     else
-	contador2=$((contador2 + 1))	
-    fi   
+	contador2=$((contador2 + 1))
+	procesos2=$((procesos2 + `cat listado.txt | head -$i | tail -1 | awk '{print $3}'`))
+    fi    
 done
 
-
-
-echo "Linux->$contador2"    
-echo "Windows->$contador"
+echo "Linux->$contador2 $procesos2"    
+echo "Windows->$contador $procesos"
 
